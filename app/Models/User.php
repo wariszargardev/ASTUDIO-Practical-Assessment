410b6e236307108164f9804da690a39351531bed
warisfullstack <146804289+warisfullstack@users.noreply.github.com>
2025-03-13 16:42:49 +0500

    Update user migration & install laravel enum package

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,10 +18,12 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
-        'password',
+        'password'
     ];
+
 
     /**
      * The attributes that should be hidden for serialization.
