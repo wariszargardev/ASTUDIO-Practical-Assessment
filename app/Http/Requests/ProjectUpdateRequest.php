6dc03c1381df863c4e0b64d34f85e52758574e29
warisfullstack <146804289+warisfullstack@users.noreply.github.com>
2025-03-17 14:25:21 +0500

    Attribute model & Attributes value model and migration setup & update some migration

--- app/Http/Requests/ProjectUpdateRequest.php
+++ app/Http/Requests/ProjectUpdateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Enums\ProjectStatus;
+use App\Models\Attribute;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ProjectUpdateRequest extends FormRequest
@@ -26,5 +27,27 @@ class ProjectUpdateRequest extends FormRequest
             'name' => 'required|string|min:5|max:255|unique:projects,name,' . $this->route('project'),
             'status' => 'required|in:' . implode(',', ProjectStatus::values()),
         ];
+        $attributes = request()['attributes'];
+        if ($this->has('attributes') && is_array($attributes)) {
+            foreach ($attributes as $attributeInfo) {
+                $attribute = Attribute::where('name', $attributeInfo['name'])->first();
+
+                if ($attribute) {
+                    $rules["attributes.*.value"] = $this->getAttributeValidationRule($attribute);
+                }
+            }
+        }
+
+        return $rules;
+    }
+
+    private function getAttributeValidationRule(Attribute $attribute): string
+    {
+        return match ($attribute->type) {
+            'text' => 'required|string|max:255',
+            'date' => 'required|date',
+            'number' => 'required|numeric',
+            'select' => 'required|string|in:option1,option2,option3',
+        };
     }
 }
